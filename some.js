788917925864794e2b0e9881290d65d6d5003562
OlaniyanEniola <olaniyaneniola0225@gmail.com>
2023-01-10 01:14:55 +0100

    refactored code

---
+++ some.js
@@ -0,0 +1,81 @@
+// gets bill input on keyup
+// billInput.addEventListener('keyup', (e) => {
+//     bill = e.target.value;
+//     if (bill < 1) {
+//         bill = '';
+//     };
+//     return bill;
+// });
+// gets bill input on keyup on change
+// billInput.addEventListener('change', (e) => {
+//     bill = e.target.value;
+//     if (bill < 1) {
+//         bill = '';
+//     };
+//     return bill;
+// });
+
+
+// gets custom tip input on keyup
+// customTipInput.addEventListener('keyup', (e) => {
+//     let custom = e.target.value;
+//     if (custom < 1) {
+//         custom = '';
+//     };
+//     return custom;
+// });
+// gets custom tip input on change
+// customTipInput.addEventListener('change', (e) => {
+//     custom = e.target.value;
+//     if (custom < 1) {
+//         custom = '';
+//     };
+//     return custom;
+// });
+
+
+// gets number of people on keyup
+// people.addEventListener('keyup', (e) => {
+//     numOfPeople = e.target.value;
+//     if (numOfPeople < 1) {
+//         numOfPeople = '';
+//     };
+//     console.log(numOfPeople);
+//     return numOfPeople;
+// });
+// gets number of people on change
+// people.addEventListener('change', (e) => {
+//     numOfPeople = e.target.value;
+//     if (numOfPeople < 1) {
+//         numOfPeople = '';
+//     };
+//     return numOfPeople;
+// });
+
+
+// if (numOfPeople == '') {
+    // tipAmountDiv.innerText = '$0.00';
+    // totalDiv.innerText = `$${bill}`;
+// };
+
+
+// calculates onkeyup
+// billInput.addEventListener('keyup', (e) => {
+//     calculateTotalPerPerson();
+// });
+// customTipInput.addEventListener('keyup', (e) => {
+//     calculateTotalPerPerson();
+// });
+// numOfpeopleInput.addEventListener('keyup', (e) => {
+//     calculateTotalPerPerson();
+// });
+// calculates onChange
+// customTipInput.addEventListener('change', (e) => {
+//     calculateTotalPerPerson();
+// });
+// numOfpeopleInput.addEventListener('change', (e) => {
+//     calculateTotalPerPerson();
+// });
+// billInput.addEventListener('change', () => {
+//     calculateTotalPerPerson();
+// });
--- src/script.js
+++ src/script.js
@@ -1,78 +1,26 @@
 const billInput = document.querySelector('.bill');
 const preRegTipInput = document.querySelectorAll('.tip');
 const customTipInput = document.querySelector('.custom');
-const people = document.querySelector('.people');
+const numOfpeopleInput = document.querySelector('.people');
 const tipAmountDiv = document.getElementById('tip-amount');
 const totalDiv = document.getElementById('total');
 const resetBtn = document.getElementById('resetBtn');
 
-// bill input and tip input are read onChange and onKeyup
 // tip input is converted to a percentage and multiplied with the bill to get the tip amount
 // tip amount is added to bill to get total 
 // total is divided by the number of people to get total per person
 // tip amount is divided by number of people to get tip amount per person
+// bill input, tip input and number of people are read onChange and onKeyup to re-calculate
 
 
-// gets bill input on keyup
-billInput.addEventListener('keyup', (e) => {
-    bill = e.target.value;
-    if (bill < 1) {
-        bill = '';
-    };
-    return bill;
-});
-// gets bill input on keyup on change
-billInput.addEventListener('change', (e) => {
-    bill = e.target.value;
-    if (bill < 1) {
-        bill = '';
-    };
-    return bill;
-});
-
-
-// gets custom tip input on keyup
-customTipInput.addEventListener('keyup', (e) => {
-    let custom = e.target.value;
-    if (custom < 1) {
-        custom = '';
-    };
-    return custom;
-});
-// gets custom tip input on change
-customTipInput.addEventListener('change', (e) => {
-    custom = e.target.value;
-    if (custom < 1) {
-        custom = '';
-    };
-    return custom;
-});
-
-
-// gets number of people on keyup
-people.addEventListener('keyup', (e) => {
-    numOfPeople = e.target.value;
-    if (numOfPeople < 1) {
-        numOfPeople = '';
-    };
-    console.log(numOfPeople);
-    return numOfPeople;
-});
-// gets number of people on change
-people.addEventListener('change', (e) => {
-    numOfPeople = e.target.value;
-    if (numOfPeople < 1) {
-        numOfPeople = '';
-    };
-    return numOfPeople;
-});
-
-
+// SLOT SLOT SLOT SLOT
 
 // calculates total per person if custom input is empty or not
-const calculateTotalPerPerson = (tip = customTipInput.value) => {
+// customTipInput is set to default parameter
+function calculateTotalPerPerson (tip = customTipInput.value) {
+    let bill = billInput.value;
     if (tip == '' || tip !== '') {
-        numOfPeople = people.value;
+        let numOfPeople = numOfpeopleInput.value;
         let tipPercentage = tip / 100;
         let tipAmount = tipPercentage * Number(bill);
         let total = tipAmount + Number(bill);
@@ -80,51 +28,30 @@ const calculateTotalPerPerson = (tip = customTipInput.value) => {
         totalDiv.innerText = `$${totalPerPerson.toFixed(2)}`;
         let tipAmountPerPerson = Number(tipAmount) / Number(numOfPeople);
         tipAmountDiv.innerText = `$${tipAmountPerPerson.toFixed(2)}`;
-        if (numOfPeople == '') {
-            tipAmountDiv.innerText = '$0.00';
-            totalDiv.innerText = `$${bill}`;
-        };
     };
 };
 
+// CALCULATION ON INTERACTION(EVENTS)
+// let bill = billInput.value;
+function calcOn ([...fields], [...events]) {
+    fields.forEach((field) => {
+        events.forEach((event) => {
+            field.addEventListener(`${event}`, (e) => {
+                calculateTotalPerPerson();
+            });          
+        });
+    });
+};
+calcOn([billInput, customTipInput, numOfpeopleInput], ['keyup', 'click']);
 
-// CALCULATION ON EVENTS
-// calculates onkeyup
-billInput.addEventListener('keyup', (e) => {
-    // set total to bill if totalPerPerson is not calculated
-    totalDiv.innerText = `$${bill}`;
-    calculateTotalPerPerson();
-});
-// calculates onChange
-billInput.addEventListener('change', () => {
-    totalDiv.innerText = `$${bill}`;
-    calculateTotalPerPerson();
-});
-
-
-// calculates onkeyup
-customTipInput.addEventListener('keyup', (e) => {
-    calculateTotalPerPerson();
-});
-// calculates onChange
-customTipInput.addEventListener('change', (e) => {
-    calculateTotalPerPerson();
-});
-
-
-// calculates onkeyup
-people.addEventListener('keyup', (e) => {
-    calculateTotalPerPerson();
-});
-// calculates onChange
-people.addEventListener('change', (e) => {
-    calculateTotalPerPerson();
-});
-
+// SLOT SLOT SLOT SLOT
 
 // gets pre-registered tip percentages and calculates onClick
+// sets customTipInput to null
+// passes preRegTipPercent as argument
 preRegTipInput.forEach((percent) => {
     percent.addEventListener('click', (e) => {
+        customTipInput.value = null;
         let preRegTipPercent = percent.getAttribute('value');
         preRegTipPercent = Number(preRegTipPercent);
         calculateTotalPerPerson(preRegTipPercent);
